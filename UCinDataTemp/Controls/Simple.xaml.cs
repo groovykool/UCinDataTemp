a3cde0847a7e5fa392feb72d5232f0a4aba0dab3
groovykool <48837150+groovykool@users.noreply.github.com>
2020-12-02 13:49:23 -0600

    Add project files.

---
+++ UCinDataTemp/Controls/Simple.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UCinDataTemp.Controls
+{
+    public sealed partial class Simple : UserControl
+    {
+        public static readonly DependencyProperty LeaderProperty = 
+            DependencyProperty.Register("Leader",
+                typeof(string),
+                typeof(Simple), new PropertyMetadata("Leader Text"));
+
+        int index = 1;
+
+        public Simple()
+        {
+            this.InitializeComponent();
+        }
+        public string Leader
+        {
+            get { return (string)GetValue(LeaderProperty); }
+            set { SetValue(LeaderProperty, value); }
+        }
+
+        private void tbutton_Click(object sender, RoutedEventArgs e)
+        {
+            follower.Text = $"Pressed {index} times.";
+            //Leader = $"Set From Code {index} times. ";
+            index++;
+        }
+    }
+}
---
+++ UCinDataTemp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Controls;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UCinDataTemp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+
+        ObservableCollection<Stupid> Source = new ObservableCollection<Stupid>();
+        private string _BLeader;
+
+        public string BLeader
+        {
+            get { return _BLeader; }
+            set { Set(ref _BLeader, value, "BLeader"); }
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BLeader = "Leader from x:Bind";
+            Source.Add(new Stupid("Leader one"));
+            Source.Add(new Stupid("Leader two"));
+            Source.Add(new Stupid("Leader three"));
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public class Stupid
+    {
+        public string Strg { get; set; }
+        public Stupid(string s)
+        {
+            Strg = s;
+        }
+    }
+}
